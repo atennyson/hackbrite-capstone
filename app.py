a33391447b0b806ccd79d8111785a31e4bf838f8
Anthony Tennyson <anthonytennyson11@gmail.com>
2023-02-23 15:35:43 -0600

    Removed redundancy in app.py where dataframe object was being made from another dataframe object

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 import db
-import pandas as pd
 
 while True:
 
-    con = db.make_connection(db.name, db.user, db.password, db.port)
+    con = db.make_connection(db.host, db.name, db.user, db.password, db.port)
     query = input("Please submit a SQL query: ")
 
     rows = db.db_query(con, query)
 
-    df = pd.DataFrame(rows)
-    print(df)
+    print(rows)
 
     result = input("Would you like to save as a csv file? Y/N: ")
 
     if result.lower() == "y":
         file_name = input("What would you like the file name to be? ")
-        df.to_csv(rf'~/Downloads/{file_name}', index=False)
+        rows.to_csv(rf'~/Downloads/{file_name}', index=False)
+        print("Your csv file will be in your downloads directory.")
 
     cont = input("Would you like to run another query? Y/N: ")
     if cont.lower() == "y":
