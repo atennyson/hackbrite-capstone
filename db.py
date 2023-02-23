82f3f396198cca8edd00d6fcc222a557b83235df
Anthony Tennyson <anthonytennyson11@gmail.com>
2023-02-23 13:45:16 -0600

    Added sqlalchemy module

--- db.py
+++ db.py
@@ -1,19 +1,22 @@
-import psycopg2
 import pandas as pd
 import os
 import dotenv
+from sqlalchemy import create_engine
 
 
 dotenv.load_dotenv("secrets.env")
 
+host = os.getenv("DBHOST")
 name = os.getenv("DBNAME")
 user = os.getenv("DBUSER")
 password = os.getenv("DBPASS")
 port = os.getenv("DBPORT")
 
 
-def make_connection(name, user, password, port):
-    con = psycopg2.connect(f"dbname={name} user={user} password={password} port={port}")
+def make_connection(host, name, user, password, port):
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{name}')
+
+    con = engine.connect()
 
     return con
 
